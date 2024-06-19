76371f652864d371083022201d72cfe8f0032d63
milesdwilliams15 <milesdwilliams15@gmail.com>
2024-06-19 16:41:08 -0400

    finished gcs for unit 1

--- _data/_code_to_make_data.R
+++ _data/_code_to_make_data.R
@@ -3,6 +3,109 @@
 #########################
 
 
+
+# election.csv ------------------------------------------------------------
+
+library(tidyverse)
+library(qss)
+library(tidycensus)
+data("elections")
+elections |>
+  filter(year %in% c(2008, 2012)) |>
+  mutate(
+    turnout = rep + dem + other
+  ) -> election
+
+v17 <- load_variables(2010, "acs5", cache = TRUE)
+
+View(v17)
+get_acs(
+  "county",
+  variables = c(
+    pop = "B01003_001"
+  ),
+  year = 2010
+) -> fl
+fl |>
+  select(-moe) |>
+  pivot_wider(
+    names_from = variable,
+    values_from = estimate
+  ) -> fl
+fl |>
+  separate(NAME, c("county", "state"), ", ") |>
+  transmute(
+    county = str_to_lower(county) |>
+      str_remove_all(" county") |>
+      str_remove_all(" parish") |>
+      str_replace_all("jeff jefferson", "jefferson") |>
+      str_replace_all("jeff ", "jefferson ") |>
+      str_replace_all(" city city", " city") |>
+      str_replace_all("st[.] ", "st ") |>
+      str_replace_all("ste ", "st ") |>
+      str_replace_all("saint ", "st ") |>
+      str_replace_all(" ", "") |>
+      str_replace_all("'", "") |>
+      str_replace_all("[.]", ""),
+    state = str_to_lower(state),
+    pop
+  ) -> fl
+election |>
+  mutate(
+    county = county |>
+      str_remove_all(" county") |>
+      str_remove_all(" parish") |>
+      str_replace_all("jeff jefferson", "jefferson") |>
+      str_replace_all("jeff ", "jefferson ") |>
+      str_replace_all(" city city", " city") |>
+      str_replace_all("st[.] ", "st ") |>
+      str_replace_all("ste[.] ", "st ") |>
+      str_replace_all("saint ", "st ") |>
+      str_replace_all(" ", "") |>
+      str_replace_all("'", "")
+  ) -> election
+
+fl$county[fl$state == "virginia" & 
+            fl$county == "manassasparkcity"] <- "manassascitypark"
+fl$county[fl$state == "new mexico" &
+            fl$county == "do?aana"] <- "doaana"
+election$county[election$state == "virginia" & 
+            election$county == "manassasparkcity"] <- "manassascitypark"
+
+election |>
+  pivot_longer(
+    rep:turnout
+  ) |>
+  mutate(
+    variable = paste0(name, year)
+  ) |>
+  select(
+    -year, -name
+  ) |>
+  pivot_wider(
+    names_from = variable,
+    values_from = value
+  ) -> election
+
+left_join(
+  election, fl
+) -> election
+
+election |>
+  mutate(
+    dem_margin08 = (dem2008 - rep2008) / (dem2008 + rep2008),
+    dem_margin12 = (dem2012 - rep2012) / (dem2012 + rep2012),
+    winner08 = ifelse(dem_margin08 > 0, "Dem (Obama)", "Rep (McCain)"),
+    winner12 = ifelse(dem_margin12 > 0, "Dem (Obama)", "Rep (Romney)"),
+    pop_decile = ntile(pop, n = 10)
+  ) -> election
+
+write_csv(
+  election,
+  here::here("_data", "election.csv")
+)
+rm(list = ls())
+
 # florida.csv and florida_map.csv -----------------------------------------
 
 library(tidyverse)
