7f485bc73c6dad138e65b2fefb7352b7bd59e5ac
milesdwilliams15 <milesdwilliams15@gmail.com>
2024-06-20 11:06:46 -0400

    d

--- _data/_code_to_make_data.R
+++ _data/_code_to_make_data.R
@@ -104,7 +104,7 @@ write_csv(
   election,
   here::here("_data", "election.csv")
 )
-rm(list = ls())
+rm(list = ls()) # clean workspace when done
 
 # florida.csv and florida_map.csv -----------------------------------------
 
@@ -170,3 +170,111 @@ map_data("county") |>
     )
   )
 rm(list = ls()) # clean workspace when done
+
+
+
+# datasets for unit 2 -----------------------------------------------------
+
+library(tidyverse)
+library(peacesciencer)
+read_csv(
+  here::here(
+    "_data", "mie-1.0.csv"
+  )
+) -> mie
+
+# aggregate the date to the event level
+
+# mie |> # is it directed?
+#   group_by(ccode1, ccode2) |>
+#   count() |>
+#   view() ## yes
+
+# are events unique?
+# mie |>
+#   group_by(micnum, eventnum) |>
+#   count() |>
+#   view() ## yes
+  
+mie |>
+  group_by(styear, stmon) |>
+  summarize(
+    events = n(),
+    warevents = sum(hostlev==5),
+    fatalmin = sum(fatalmin1 + fatalmin2),
+    fatalmax = sum(fatalmax1 + fatalmax2),
+    warfatalmin = sum(fatalmin1[hostlev == 5] +
+                      fatalmin2[hostlev == 5]),
+    warfatalmax = sum(fatalmax1[hostlev == 5] +
+                        fatalmax2[hostlev == 5])
+  ) -> event_count_series
+
+write_csv(
+  event_count_series,
+  here::here(
+    "_data", "intl_conflict.csv"
+  )
+)
+
+# make a dataset of countries involved in events
+
+mie |>
+  select(styear, stmon, ccode1, ccode2) |>
+  pivot_longer(
+    ccode1:ccode2
+  ) |>
+  select(-name) |>
+  distinct() |>
+  mutate(
+    involved_in_conflict = 1  
+  )-> conflict_countries
+
+
+create_stateyears(
+  subset_years = 1816:2014
+) -> cyd
+
+cyd |>
+  full_join(
+    conflict_countries, by = c("ccode" = "value",
+                               "year" = "styear")
+  ) |>
+  drop_na()->conflict_countries
+
+write_csv(
+  conflict_countries,
+  here::here(
+    "_data",
+    "conflict_countries.csv"
+  )
+)
+
+
+cyd |>
+  add_sdp_gdp() |>
+  transmute(
+    statenme,
+    year,
+    pop = exp(wbpopest),
+    gdp = exp(wbgdp2011est)
+  ) -> cpop
+
+write_csv(
+  cpop,
+  here::here(
+    "_data",
+    "country_pop_gdp.csv"
+  )
+)
+
+cyd |>
+  add_democracy() -> dem
+
+write_csv(
+  dem,
+  here::here(
+    "_data",
+    "democracy.csv"
+  )
+)
+rm(list = ls()) ## clean up
